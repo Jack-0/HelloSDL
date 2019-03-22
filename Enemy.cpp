de11e27aea90aba70e9e6513721662d781b6d44f
jack <jack.jh@protonmail.com>
2019-03-22 14:32:00 +0000

    commented Enemy code

--- Enemy.cpp
+++ Enemy.cpp
@@ -12,20 +12,28 @@ Enemy::Enemy(const LoaderParams *pParams, int max)
 
 void Enemy::update()
 {
-    changeFrame(4, 200);
-    ttd += 1;
+    // if the Enemy is not dead accelerate right, else accelerate down
+    if(!dead)
+        setXacceleration(0.005);
+    else
+        setYacceleration(0.2);
 
+    SDLGameObject::update(); // This must be called to use vector logic
+
+    changeFrame(4, 200);
+    ttd += 1; // increase time to die value
+
+    // if it's time to die, then die
     if(ttd >= max_ttd)
     {
         dead = true;
         return;
     }
-
-    moveRight();
 }
 
 void Enemy::draw()
 {
+    // if dead render the death frames at a set speed, else render the move animation
     if(dead)
     {
         if(deathTime > 40)
